94529b115512077719e28bf3b4434c743b93414e
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2018-04-11 13:49:09 +0100

    First commit

---
+++ lab_enumeration_start_point/enumeration_library.js
@@ -0,0 +1,26 @@
+const EnumerationLibrary = function(array) {
+  this.array = array
+}
+
+EnumerationLibrary.prototype.find = function(callback) {
+
+}
+
+EnumerationLibrary.prototype.map = function(callback) {
+
+}
+
+EnumerationLibrary.prototype.filter = function(callback) {
+
+}
+
+EnumerationLibrary.prototype.some = function(callback) {
+
+}
+
+EnumerationLibrary.prototype.every = function(callback) {
+  
+}
+
+
+module.exports = EnumerationLibrary;
